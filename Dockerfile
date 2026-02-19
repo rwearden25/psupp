5b1556fb711c9f2d4fc4535af4d7b7cb2c6201c1
Ross Wearden <ross.wearden@toshibagcs.com>
2026-02-19 12:50:40 -0600

    Switch to Node.js server

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM nginx:alpine
-
-COPY nginx.conf /etc/nginx/conf.d/default.conf
-COPY index.html /usr/share/nginx/html/
-COPY knowledge-base.js /usr/share/nginx/html/
-
-CMD sed -i "s/RAILWAY_PORT/$PORT/g" /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'application/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.ico': 'image/x-icon'
+};
+
+http.createServer((req, res) => {
+  let filePath = req.url === '/' ? '/index.html' : req.url;
+  filePath = path.join(__dirname, filePath);
+  const ext = path.extname(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': mimeTypes[ext] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(PORT, () => {
+  console.log(`P-Supp running on port ${PORT}`);
+});
